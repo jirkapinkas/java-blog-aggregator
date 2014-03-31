18c268cbd84202669588eb8125c572d139b4090b
jirkapinkas <jirka.pinkas@gmail.com>
2014-03-31 10:32:25 +0200

    clob and column length

--- src/main/java/cz/jiripinkas/jba/entity/Blog.java
+++ src/main/java/cz/jiripinkas/jba/entity/Blog.java
@@ -3,6 +3,7 @@ package cz.jiripinkas.jba.entity;
 import java.util.List;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -22,6 +23,7 @@ public class Blog {
 
 	@Size(min = 1, message = "Invalid URL!")
 	@URL(message = "Invalid URL!")
+	@Column(length = 1000)
 	private String url;
 
 	@Size(min = 1, message = "Name must be at least 1 character!")
--- src/main/java/cz/jiripinkas/jba/entity/Item.java
+++ src/main/java/cz/jiripinkas/jba/entity/Item.java
@@ -7,7 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.Type;
 
 @Entity
 public class Item {
@@ -16,13 +19,18 @@ public class Item {
 	@GeneratedValue
 	private Integer id;
 
+	@Column(length = 1000)
 	private String title;
 
+	@Lob
+	@Type(type = "org.hibernate.type.StringClobType")
+	@Column(length = Integer.MAX_VALUE)
 	private String description;
 
 	@Column(name = "published_date")
 	private Date publishedDate;
 
+	@Column(length = 1000)
 	private String link;
 
 	@ManyToOne
