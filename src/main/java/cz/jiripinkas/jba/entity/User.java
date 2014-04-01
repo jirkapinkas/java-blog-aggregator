a8c313655ef421007b99028f538acb093feba974
jirkapinkas <jirka.pinkas@gmail.com>
2014-04-01 13:02:44 +0200

    production db

--- src/main/java/cz/jiripinkas/jba/entity/User.java
+++ src/main/java/cz/jiripinkas/jba/entity/User.java
@@ -10,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.Email;
@@ -17,6 +18,7 @@ import org.hibernate.validator.constraints.Email;
 import cz.jiripinkas.jba.annotation.UniqueUsername;
 
 @Entity
+@Table(name = "app_user")
 public class User {
 
 	@Id
--- src/main/java/cz/jiripinkas/jba/service/InitDbService.java
+++ src/main/java/cz/jiripinkas/jba/service/InitDbService.java
@@ -38,44 +38,47 @@ public class InitDbService {
 
 	@PostConstruct
 	public void init() {
-		Role roleUser = new Role();
-		roleUser.setName("ROLE_USER");
-		roleRepository.save(roleUser);
+		if (roleRepository.findByName("ROLE_ADMIN") == null) {
+			Role roleUser = new Role();
+			roleUser.setName("ROLE_USER");
+			roleRepository.save(roleUser);
 
-		Role roleAdmin = new Role();
-		roleAdmin.setName("ROLE_ADMIN");
-		roleRepository.save(roleAdmin);
+			Role roleAdmin = new Role();
+			roleAdmin.setName("ROLE_ADMIN");
+			roleRepository.save(roleAdmin);
 
-		User userAdmin = new User();
-		userAdmin.setEnabled(true);
-		userAdmin.setName("admin");
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		userAdmin.setPassword(encoder.encode("admin"));
-		List<Role> roles = new ArrayList<Role>();
-		roles.add(roleAdmin);
-		roles.add(roleUser);
-		userAdmin.setRoles(roles);
-		userRepository.save(userAdmin);
+			User userAdmin = new User();
+			userAdmin.setEnabled(true);
+			userAdmin.setName("admin");
+			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+			userAdmin.setPassword(encoder.encode("admin"));
+			List<Role> roles = new ArrayList<Role>();
+			roles.add(roleAdmin);
+			roles.add(roleUser);
+			userAdmin.setRoles(roles);
+			userRepository.save(userAdmin);
 
-		Blog blogJavavids = new Blog();
-		blogJavavids.setName("JavaVids");
-		blogJavavids.setUrl("http://feeds.feedburner.com/javavids?format=xml");
-		blogJavavids.setUser(userAdmin);
-		blogRepository.save(blogJavavids);
+			Blog blogJavavids = new Blog();
+			blogJavavids.setName("JavaVids");
+			blogJavavids
+					.setUrl("http://feeds.feedburner.com/javavids?format=xml");
+			blogJavavids.setUser(userAdmin);
+			blogRepository.save(blogJavavids);
 
-//		Item item1 = new Item();
-//		item1.setBlog(blogJavavids);
-//		item1.setTitle("first");
-//		item1.setLink("http://www.javavids.com");
-//		item1.setPublishedDate(new Date());
-//		itemRepository.save(item1);
-//
-//		Item item2 = new Item();
-//		item2.setBlog(blogJavavids);
-//		item2.setTitle("second");
-//		item2.setLink("http://www.javavids.com");
-//		item2.setPublishedDate(new Date());
-//		itemRepository.save(item2);
+			// Item item1 = new Item();
+			// item1.setBlog(blogJavavids);
+			// item1.setTitle("first");
+			// item1.setLink("http://www.javavids.com");
+			// item1.setPublishedDate(new Date());
+			// itemRepository.save(item1);
+			//
+			// Item item2 = new Item();
+			// item2.setBlog(blogJavavids);
+			// item2.setTitle("second");
+			// item2.setLink("http://www.javavids.com");
+			// item2.setPublishedDate(new Date());
+			// itemRepository.save(item2);
+		}
 
 	}
 }
