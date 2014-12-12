e9abf63efb3dbd3135fd5c16eeb0a1d9d753301f
Jiri Pinkas <jirka.pinkas@gmail.com>
2014-12-12 05:39:23 +0100

    typo

--- src/main/java/cz/jiripinkas/jba/controller/BlogIconController.java
+++ src/main/java/cz/jiripinkas/jba/controller/BlogIconController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +18,7 @@ public class BlogIconController {
 	@Autowired
 	private BlogService blogService;
 
-	@RequestMapping("/icon/{blogId}")
+	@RequestMapping(value = "/icon/{blogId}", produces = MediaType.IMAGE_PNG_VALUE)
 	public @ResponseBody byte[] getBlogIcon(@PathVariable int blogId) throws IOException {
 		byte[] icon = blogService.getIcon(blogId);
 		if (icon == null) {
