35cc58c6bee80015d02853e659e005ab6ffd62e5
Jiri Pinkas <jirka.pinkas@gmail.com>
2015-03-17 15:52:02 +0100

    more intelligent updates during loading new items

--- src/main/java/cz/jiripinkas/jba/repository/BlogRepository.java
+++ src/main/java/cz/jiripinkas/jba/repository/BlogRepository.java
@@ -3,6 +3,7 @@ package cz.jiripinkas.jba.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import cz.jiripinkas.jba.entity.Blog;
@@ -25,4 +26,13 @@ public interface BlogRepository extends JpaRepository<Blog, Integer>{
 	List<Blog> findAllFetchUser();
 
 	Blog findByIdAndUserName(int id, String username);
+	
+	@Modifying
+	@Query("update Blog b set b.lastCheckErrorText = null, b.lastCheckErrorCount = 0, b.lastCheckStatus = true where b.id = ?1")
+	void saveOk(int blogId);
+
+	@Modifying
+	@Query("update Blog b set b.lastCheckErrorText = ?3, b.lastCheckErrorCount = ?2, b.lastCheckStatus = false where b.id = ?1")
+	void saveFail(int blogId, int errorCount, String errorText);
+
 }
--- src/main/java/cz/jiripinkas/jba/service/BlogResultService.java
+++ src/main/java/cz/jiripinkas/jba/service/BlogResultService.java
@@ -16,22 +16,17 @@ public class BlogResultService {
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void saveOk(Blog blog) {
-		blog.setLastCheckErrorText(null);
-		blog.setLastCheckErrorCount(0);
-		blog.setLastCheckStatus(true);
-		blogRepository.save(blog);
+		blogRepository.saveOk(blog.getId());
 	}
 
 	@Transactional(propagation = Propagation.REQUIRES_NEW)
 	public void saveFail(Blog blog, String errorText) {
-		blog.setLastCheckErrorText(errorText);
 		int errorCount = 0;
-		if(blog.getLastCheckErrorCount() != null) {
+		if (blog.getLastCheckErrorCount() != null) {
 			errorCount = blog.getLastCheckErrorCount();
 		}
 		blog.setLastCheckErrorCount(errorCount + 1);
-		blog.setLastCheckStatus(false);
-		blogRepository.save(blog);
+		blogRepository.saveFail(blog.getId(), errorCount, errorText);
 	}
 
 }
