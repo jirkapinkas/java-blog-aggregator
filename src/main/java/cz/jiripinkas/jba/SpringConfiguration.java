28986ab1fc74ad4a506ae247186c4589c1d3a3a4
Jiri Pinkas <jirka.pinkas@gmail.com>
2015-09-01 10:45:10 +0200

    moved some stuff to Java Config configuration

--- src/main/java/cz/jiripinkas/jba/SpringConfiguration.java
+++ src/main/java/cz/jiripinkas/jba/SpringConfiguration.java
@@ -1,10 +1,38 @@
 package cz.jiripinkas.jba;
 
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.dozer.DozerBeanMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
+@EnableTransactionManagement
+@EnableJpaRepositories("cz.jiripinkas.jba.repository")
 @ComponentScan
 @Configuration
 public class SpringConfiguration {
+	
+	@Bean
+	public JpaTransactionManager transactionManager(DataSource dataSource, EntityManagerFactory entityManagerFactory) {
+		JpaTransactionManager transactionManager = new JpaTransactionManager(entityManagerFactory);
+		transactionManager.setDataSource(dataSource);
+		return transactionManager;
+	}
+	
+	@Bean
+	public DozerBeanMapper dozerBeanMapper() {
+		return new DozerBeanMapper();
+	}
 
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
 }
--- src/main/java/cz/jiripinkas/jba/SpringDevConfiguration.java
+++ src/main/java/cz/jiripinkas/jba/SpringDevConfiguration.java
@@ -1,11 +1,43 @@
 package cz.jiripinkas.jba;
 
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.hibernate.jpa.HibernatePersistenceProvider;
+import org.hsqldb.jdbc.JDBCDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 
 import cz.jiripinkas.jba.annotation.DevProfile;
 
 @DevProfile
 @Configuration
 public class SpringDevConfiguration {
+
+	@Bean
+	public DataSource dataSource() {
+		JDBCDataSource dataSource = new JDBCDataSource();
+		dataSource.setUrl("jdbc:hsqldb:mem:test;sql.syntax_pgs=true");
+		dataSource.setUser("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
+
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setDataSource(dataSource);
+		Properties jpaProperties = new Properties();
+		jpaProperties.put("hibernate.hbm2ddl.auto", "create");
+		jpaProperties.put("hibernate.show_sql", "true");
+		jpaProperties.put("hibernate.default_batch_fetch_size", 10);
+		entityManagerFactory.setJpaProperties(jpaProperties);
+		entityManagerFactory.setPackagesToScan("cz.jiripinkas.jba.entity");
+		entityManagerFactory.setPersistenceProvider(new HibernatePersistenceProvider());
+		return entityManagerFactory;
+	}
+
 
 }
